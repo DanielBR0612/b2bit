1a83a9b48e6768861d6605cabdee829ecb9b52d0
Daniel Braga <danielfranciscobragadearaujo@gmail.com>
2025-09-23 11:11:34 -0300

    feat: login page

---
+++ b2bit/src/components/CardLogin.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Logo } from "@/components/Logo";
+
+export function CardLogin() {
+  return (
+    <Card className="h-full w-full max-w-sm shadow-2xl">
+      <CardHeader>
+        <Logo></Logo>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">E-mail</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="@gmail.com"
+                required
+                className="bg-gray-100"
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input id="password" type="password" required className="bg-gray-100" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button type="submit" className="w-full bg-b2bit-blue text-white hover:bg-b2bit-blue-100">
+          Sign-in
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
